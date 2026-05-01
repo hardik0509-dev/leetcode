8f330aeb20fb3cb78f964a5fa591d290c41ee24e
hardik0509-dev <hardik5sept2006@gmail.com>
2026-05-01 23:21:26 +0530

    Time: 4 ms (22.90%) | Memory: 9.5 MB (27.57%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,char>mp1;
+         unordered_map<char,char>mp2;
+         for(int i=0;i<s.length();i++){
+            char ch1=s[i];
+            char ch2=t[i];
+             
+             if(mp1.count(ch1)){
+                if(mp1[ch1]!=ch2) return false;
+             }
+             else{
+                mp1[ch1]=ch2;
+             }
+             if(mp2.count(ch2)){
+                if(mp2[ch2]!=ch1) return false;
+             }
+             else{
+                mp2[ch2]=ch1;
+             }
+         }
+
+        
+      return true;
+        
+    }
+};
